415afeaabae891634d80b19f0d30f9a5e13d5d06
shihao-hub <2958017271@qq.com>
2026-09-17 23:21:32 +0800

    chore: 移除已恢复项目 sublime-folders 的归档副本

--- .archived/go_projects/sublime-folders/alert_other.go
+++
@@ -1,9 +0,0 @@
-//go:build !windows
-
-package app
-
-import "log"
-
-func AlertError(title, text string) {
-	log.Printf("%s: %s", title, text)
-}
--- .archived/go_projects/sublime-folders/mutex_other.go
+++
@@ -1,5 +0,0 @@
-//go:build !windows
-
-package app
-
-func AcquireSingleInstance() bool { return true }
